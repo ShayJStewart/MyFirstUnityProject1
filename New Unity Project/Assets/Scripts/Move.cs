3765ec64729b4f010b41966791fa2fbca5ff5a6f
Shayla Stewart <silverwingshay@gmail.com>
2017-02-02 09:59:39 -0700

    C# updated scripts
    I went in and updated the comments with the C# scripts.

--- New Unity Project/Assets/Scripts/Move.cs
+++ New Unity Project/Assets/Scripts/Move.cs
@@ -5,16 +5,20 @@ public class Move : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		//codes for moving the objct up down lft and right using the arrow keys
+		//codes for moving the objct up down left and right using the arrow keys
+		//code used to move the player left on the X axis using the left arrow
 		if (Input.GetKey (KeyCode.LeftArrow)) {
 			gameObject.transform.Translate (-.5f, 0, 0);
 		}
+		//code for moving the player right in the X axis using right arrow
 		if (Input.GetKey (KeyCode.RightArrow)) {
 			gameObject.transform.Translate (.5f, 0, 0);
 		}
+		// code for moving player up in the Y axis using the up arrow
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			gameObject.transform.Translate (0, .5f, 0);
 		}
+		//code for moving player down in the Y axis using the down arrow
 		if (Input.GetKey (KeyCode.DownArrow)) {
 			gameObject.transform.Translate (0, -.5f, 0);
 		}
