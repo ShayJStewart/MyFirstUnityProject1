335b26601c82ec779f590999ba193d5f5e49230f
Shayla Stewart <silverwingshay@gmail.com>
2017-03-01 20:44:16 -0700

    ZooRoster Homework
    hahaha,  I did it!!! So happy and proud! Simpler than I thought:)

---
+++ New Unity Project/Assets/Scripts/AnimalListshomework.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalListshomework : MonoBehaviour {
+
+	public string animal;
+	public List<string> zooRoster = new List<string>();
+	
+	// Update is called once per frame
+	void Start () {
+		zooRoster.Add("monkey");
+		zooRoster.Add("elephant");
+		zooRoster.Add("Zebra");
+	}
+
+
+	public void AddAnimal()
+	{
+		if (animal != "") 
+		{
+			if (zooRoster.Contains (animal) != true) 
+			{
+				zooRoster.Add(animal);
+			}
+		}
+	}
+
+	public void RemoveAnimal()
+	{
+		if (animal != "") 
+		{
+			if (zooRoster.Contains (animal) == true)
+			{
+				zooRoster.Remove (animal);
+			}
+		}
+		/*if (animal == selected
+		zooRoster.Remove(animal);*/
+	}
+
+	/*public void PrintAll()
+	{
+		print (" Your zoo list :" + zooRoster[(0, zooRoster.Length)]);
+	}*/
+
+	public void ClearAll()
+	{
+		zooRoster.Clear();
+		print ("No animals to show");
+	}
+
+	public void PrintAll()
+	{
+		print ("Zoo Roster:");
+
+		foreach (var animal in zooRoster)
+		{
+			print(animal);
+		}
+	}
+
+}
---
+++ New Unity Project/Assets/Scripts/ListsandFunctions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ListsandFunctions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		CountToTen ();
+		print ("---------");
+		CountToTen ();
+		print ("---------");
+		CountToTen ();
+		print ("---------");
+		CountToTen ();
+		
+	}
+	
+	void CountToTen()
+	{
+		int i = 1;
+		while (i <= 10) 
+		{
+			print (i);
+			i++;
+		}
+		print ("Congrats! You counted to 10!");
+	}
+}
