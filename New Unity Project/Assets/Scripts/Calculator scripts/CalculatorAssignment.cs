d32ef7f9f57770cecfd635947eb74aad53cc6691
Shayla Stewart <silverwingshay@gmail.com>
2017-04-25 21:22:41 -0600

    Figuring out freezing
    Im trying to figure out how to freeze the numbers left on the screen when it goes game over

--- New Unity Project/Assets/Scripts/Calculator scripts/CalculatorAssignment.cs
+++ New Unity Project/Assets/Scripts/Calculator scripts/CalculatorAssignment.cs
@@ -126,8 +126,7 @@ public class CalculatorAssignment : MonoBehaviour {
 	}
 
 
-
-
+		
 	// Update is called once per frame
 	void Update () {
 		//transform.Translate (Vector3.down * fallSpeed * Time.deltaTime, Space.World);
